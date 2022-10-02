c2241e8838c8f388daca0b540cff13e4eb7d1846
Ilya Schukin <mayksik@gmail.com>
2022-10-02 18:38:57 +0300

    Added condition

--- Porogram/Program.cs
+++ Porogram/Program.cs
@@ -1,5 +1,13 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из сторк, длинна которых меньше либо равна 3 символа. 
 
-string[] arrayString  = {"AAAA", "SSS", "DD", "FFFFF","X"};
+string[] arrayString = { "AAAA", "SSS", "DD", "FFFFF", "X" };
 string[] finalArray = new string[arrayString.Length];
+
+for (int i = 0; i < arrayString.Length; i++)
+{
+    if (arrayString[i].Length <= 3)
+    {
+        finalArray[i] = arrayString[i];
+    }
+}
 
